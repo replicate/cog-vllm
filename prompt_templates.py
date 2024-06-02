766b534f0331004697c5939ccd6dca0666bfd496
technillogue <technillogue@gmail.com>
2024-06-02 10:56:44 +0100

    Syl/mattt/train (#9)
    * minor cleanups
    
    * fix remove_system_prompt_input making predict() appear to be sync

--- prompt_templates.py
+++ prompt_templates.py
@@ -1,4 +1,4 @@
 COMPLETION = "<s>{prompt}"
-LLAMA_3_INSTRUCT = "<s><|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+LLAMA_3_INSTRUCT = "<s><|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
 LLAMA_2_INSTRUCT = "<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{prompt} [/INST]"
 MISTRAL_INSTRUCT = "<s>[INST] {system_prompt} {prompt} [/INST]"
