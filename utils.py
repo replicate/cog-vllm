3b643df08051c47d950f0c94a3db75aa493d02a4
technillogue <technillogue@gmail.com>
2024-07-23 05:36:30 -0400

    copy maybe_download_tarball_with_pget from cog-triton to use the behavior of downloading to /weights if possible and then symlinking

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import os
+import shutil
 import subprocess
 import time
 import warnings
+from pathlib import Path
 from urllib.parse import urlparse
 
 
@@ -37,6 +39,54 @@ async def resolve_model_path(url_or_local_path: str) -> str:
     else:
         raise ValueError(f"E1000: Unsupported model path scheme: {parsed_url.scheme}")
 
+def maybe_download_tarball_with_pget(
+    url: str,
+    dest: str,
+):
+    """
+    Downloads a tarball from url and decompresses to dest if dest does not exist. Remote path is constructed
+    by concatenating remote_path and remote_filename. If remote_path is None, files are not downloaded.
+
+    Args:
+        url (str): URL to the tarball
+        dest (str): Path to the directory where the tarball should be decompressed
+
+    Returns:
+        path (str): Path to the directory where files were downloaded
+
+    """
+    try:
+        Path("/weights").mkdir(exist_ok=True)
+        first_dest = "/weights/vllm"
+    except PermissionError:
+        print("/weights doesn't exist, and we couldn't create it")
+        first_dest = dest
+
+
+    # if dest exists and is not empty, return
+    if os.path.exists(first_dest) and os.listdir(first_dest):
+        print(f"Files already present in `{first_dest}`, nothing will be downloaded.")
+        if first_dest != dest:
+            try:
+                os.symlink(first_dest, dest)
+            except FileExistsError:
+                print(f"Ignoring existing file at {dest}")
+        return dest
+
+    # if dest exists but is empty, remove it so we can pull with pget
+    if os.path.exists(first_dest):
+        shutil.rmtree(first_dest)
+
+    print("Downloading model assets...")
+    start_time = time.time()
+    command = ["pget", url, first_dest, "-x"]
+    subprocess.check_call(command, close_fds=True)
+    print(f"Downloaded model assets in {time.time() - start_time:.2f}s")
+    if first_dest != dest:
+        os.symlink(first_dest, dest)
+
+    return dest
+
 
 async def download_tarball(url: str) -> str:
     """
@@ -49,14 +99,6 @@ async def download_tarball(url: str) -> str:
         str: Path to the directory where the tarball was extracted.
     """
     filename = os.path.splitext(os.path.basename(url))[0]
-    path = os.path.join(os.getcwd(), "models", filename)
-    if os.path.exists(path) and os.listdir(path):
-        print(f"Files already present in `{path}`.")
-        return path
+    dest = os.path.join(os.getcwd(), "models", filename)
+    return maybe_download_tarball_with_pget(url, dest)
 
-    print(f"Downloading model assets to {path}...")
-    start_time = time.time()
-    command = ["pget", url, path, "-x"]
-    subprocess.check_call(command, close_fds=True)
-    print(f"Downloaded model assets in {time.time() - start_time:.2f}s")
-    return path
